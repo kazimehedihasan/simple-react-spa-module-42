d0a6794733221cb90209302a80a8b7eb42612d74
kazimehedihasan <kazimehedihasan458@.com>
2023-09-12 15:58:30 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
 
 
   return (
     <>
-      <h1 className='text-6xl'>Knowledge Cafe</h1>
-    
+ <Header></Header>
+    <Blogs></Blogs>
     </>
   )
 }
